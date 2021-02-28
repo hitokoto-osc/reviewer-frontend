86d24f4ee994caed4af976f4836f7d2507b5f142
a632079 <i@a632079.me>
2021-02-28 23:24:05 +0800

    fix: lint

--- components/SearchModal.vue
+++ components/SearchModal.vue
@@ -11,26 +11,10 @@
     <p>句子：{{ sentence }}</p>
     <p><strong>单击下方的按钮以使用对应的搜索引擎进行搜索。</strong></p>
     <div class="search-button-group">
-      <a-button
-        @click="openBaidu"
-      >
-        百度
-      </a-button>
-      <a-button
-        @click="openGoogle"
-      >
-        谷歌
-      </a-button>
-      <a-button
-        @click="openBing"
-      >
-        必应
-      </a-button>
-      <a-button
-        @click="openDuckDuckGo"
-      >
-        DuckDuckGo
-      </a-button>
+      <a-button @click="openBaidu"> 百度 </a-button>
+      <a-button @click="openGoogle"> 谷歌 </a-button>
+      <a-button @click="openBing"> 必应 </a-button>
+      <a-button @click="openDuckDuckGo"> DuckDuckGo </a-button>
     </div>
   </a-modal>
 </template>
@@ -45,27 +29,35 @@ export default {
   },
   computed: {
     Visible: {
-      get () {
+      get() {
         return this.visible
       },
-      set (val) {
-      // grants_改变由父组件控制
+      set(val) {
+        // grants_改变由父组件控制
         this.$emit('on-change-visible', val)
       }
     }
   },
   methods: {
-    openBaidu () {
-      window.open('https://www.baidu.com/baidu?word=' + encodeURIComponent(this.sentence))
+    openBaidu() {
+      window.open(
+        'https://www.baidu.com/baidu?word=' + encodeURIComponent(this.sentence)
+      )
     },
-    openGoogle () {
-      window.open('http://www.google.com/search?q=' + encodeURIComponent(this.sentence))
+    openGoogle() {
+      window.open(
+        'http://www.google.com/search?q=' + encodeURIComponent(this.sentence)
+      )
     },
-    openBing () {
-      window.open('https://cn.bing.com/search?q=' + encodeURIComponent(this.sentence))
+    openBing() {
+      window.open(
+        'https://cn.bing.com/search?q=' + encodeURIComponent(this.sentence)
+      )
     },
-    openDuckDuckGo () {
-      window.open('https://duckduckgo.com/?q=' + encodeURIComponent(this.sentence))
+    openDuckDuckGo() {
+      window.open(
+        'https://duckduckgo.com/?q=' + encodeURIComponent(this.sentence)
+      )
     }
   }
 }

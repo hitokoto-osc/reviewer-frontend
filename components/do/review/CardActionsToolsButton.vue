85b271fc2a86674c75db75098867722a1f027355
greenhat616 <i@a632079.me>
2023-09-11 16:37:30 +0800

    fix: icon position issue in more tools button

--- components/do/review/CardActionsToolsButton.vue
+++ components/do/review/CardActionsToolsButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-button class=":uno: flex items-center justify-center my-1">
+  <a-button class=":uno: !inline-flex items-center justify-center my-1">
     工具
     <div class=":uno: i-solar-menu-dots-bold inline-block ml-2 h-4 w-4" />
   </a-button>

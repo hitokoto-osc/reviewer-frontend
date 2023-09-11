9acef84d574d53f49817bf629beec8ef2ea8dde8
greenhat616 <i@a632079.me>
2023-09-11 20:46:06 +0800

    fix: styles

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -38,12 +38,12 @@ const userStore = useUserStore()
 .header-container {
   @apply h-16 w-full flex justify-center border-b-1 shadow-sm shadow-slate-200 shrink-0;
   @apply dark:border-dark-500 dark:shadow-dark-500 dark:text-white dark:text-opacity-85;
-  @apply fixed top-0 md:relative z-99999;
+  @apply fixed top-0 md:relative z-1000;
 
   .header-background {
-    @apply absolute left-0 top-0 h-16 w-full -z-9999;
-    @apply bg-white bg-opacity-70 backdrop-saturate-50 backdrop-blur-8;
-    @apply dark:bg-dark-700 dark:bg-opacity-70;
+    @apply absolute left-0 top-0 h-16 w-full -z-1000;
+    @apply bg-white bg-opacity-70 backdrop-saturate-50 backdrop-blur-8 md:bg-opacity-100;
+    @apply dark:bg-dark-700 dark:bg-opacity-70 dark:md:bg-opacity-100;
   }
 
   .header {
